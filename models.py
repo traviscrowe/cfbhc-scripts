c58d16c2850a544e1eef7eafc25e59308e5af944
Travis Crowe <traviscrowe@gmail.com>
2015-07-10 08:57:37 -0500

    Moved player models to models.py and added runner/reciever

---
+++ models.py
@@ -0,0 +1,73 @@
+#TODO: implement Player base type
+
+class Passer():
+
+	#Serialize into a passer object - expects 20 columns
+	def __init__(self, cols = []):
+		self.position = cols[0]
+		self.firstName = cols[1]
+		self.lastName = cols[2]
+		self.height = cols[3]
+		self.weight = int(cols[4])
+		self.year = cols[5]
+		self.skill = int(cols[6])
+		self.team = cols[7]
+		self.games = int(cols[8])
+		self.completions = int(cols[9])
+		self.attempts = int(cols[10])
+		self.percentage = float(cols[11])
+		self.yards = int(cols[12])
+		self.tds = int(cols[13])
+		self.ints = int(cols[14])
+		self.rating = float(cols[15])
+		self.ypa = float(cols[16])
+		self.ypg = float(cols[17])7
+		self.tdpct = float(cols[18])
+		self.intpct = float(cols[19])
+	def to_JSON(self):
+		return json.dumps(self.__dict__)
+
+class Runner():
+
+	#Serialize into runner object - expects 16 columns
+	def __init__(self, cols = []):
+		self.position = cols[0]
+		self.firstName = cols[1]
+		self.lastName = cols[2]
+		self.height = cols[3]
+		self.weight = int(cols[4])
+		self.year = cols[5]
+		self.skill = int(cols[6])
+		self.team = cols[7]
+		self.games = int(cols[8])
+		self.attempts = int(cols[9])
+		self.yards = int(cols[10])
+		self.tds = int(cols[11])
+		self.fumbles = int(cols[12])
+		self.hyg = int(cols[13])
+		self.ypc = float[cols[14]]
+		self.ypg = float[cols[15]]
+	def to_JSON(self):
+		return json.dumps(self.__dict__)
+
+class Reciever():
+
+	#Serialize into reciever object - expects 15 columns
+	def __init__(self, cols = []):
+		self.position = cols[0]
+		self.firstName = cols[1]
+		self.lastName = cols[2]
+		self.height = cols[3]
+		self.weight = int(cols[4])
+		self.year = cols[5]
+		self.skill = int(cols[6])
+		self.team = cols[7]
+		self.games = int(cols[8])
+		self.catches = int(cols[9])
+		self.yards = int(cols[10])
+		self.tds = int(cols[11])
+		self.fumbles = int(cols[12])
+		self.ypc = float(cols[13])
+		self.ypg = float(cols[14])
+	def to_JSON(self):
+		return json.dumps(self. __dict__)
