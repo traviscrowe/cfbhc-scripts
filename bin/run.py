482ffaaaf82eba8e996cb8ac909516d80de69b5c
Travis Crowe <traviscrowe@gmail.com>
2016-06-03 09:52:03 -0500

    fix: removed ref to broken progress bar, added CFBHC and 2018 parsing, general code quality

--- bin/run.py
+++ bin/run.py
@@ -1,57 +1,61 @@
-import util, query_data, csv, pdb
+"""
+Primary parsing script - fetches 2014 - 2018 CFBHC and NFLHC stats from the wiki
+"""
 from collections import OrderedDict
+import csv
+import util
+import query_data
 
-#currently works with each NFL and CFB table
-LEAGUES = ["NFLHC"]
-YEARS = ["2014", "2015", "2016", "2017"]
+
+LEAGUES = ["CFBHC", "NFLHC"]
+YEARS = ["2014", "2015", "2016", "2017", "2018"]
 TABLES = [1, 2, 3, 4, 5, 6]
 
-max = len(LEAGUES) * len(YEARS) * len(TABLES)
-current = 0
+PARAMS = [(l, y, t) for l in LEAGUES for y in YEARS for t in TABLES]
+for (l, y, t) in PARAMS:
+	# Use these to select the league, year, and stat table desired
+    # (1 = QB, 2 = RB, 3 = WR, 4 = DEF, 5 = K, 6 = KR)
+    LEAGUE_SELECTED = l
+    YEAR_SELECTED = y
+    TABLE_SELECTED = t
 
-grid = [(l, y, t) for l in LEAGUES for y in YEARS for t in TABLES]
-for (l, y, t) in grid:
-	#use these to select the league, year, and stat table desired (1 = QB, 2 = RB, 3 = WR, 4 = DEF, 5 = K, 6 = KR)
-	LEAGUE_SELECTED = l
-	YEAR_SELECTED = y
-	TABLE_SELECTED = t
+    if l == 'NFLHC' and y == '2014' and t == 6:
+        continue
 
-	if l == 'NFLHC' and y == '2014' and t == 6: continue
+    # Build query object
+    data = query_data.Data(LEAGUE_SELECTED, YEAR_SELECTED, TABLE_SELECTED)
 
-	#build query object
-	data = query_data.Data(LEAGUE_SELECTED, YEAR_SELECTED, TABLE_SELECTED)
+    # Get valid file name
+    file_name = util.get_csv_file(data)
 
-	#get valid file name
-	file_name = util.get_csv_file(data)
+    # Get raw player data
+    table_header = util.parse_table_header(data)
+    player_data = util.parse_table_data(data)
 
-	#get raw player data
-	table_header = util.parse_table_header(data)
-	player_data = util.parse_table_data(data)
+    # Chunk data into lists
+    if len(table_header) == 0:
+        continue
 
-	#chunk data into lists
-	players = util.chunk_player_data(player_data, len(table_header))
+    players = util.chunk_player_data(player_data, len(table_header))
 
-	#push to an ordered dictionary, then write each player record to csv
-	with open(file_name, 'w') as outfile:
-		writer = csv.writer(outfile)
-		player_dict = []
-		for player in players:
-			tmp = OrderedDict()
-			for index in range(len(player)):
-				if type(player[index].text) is float:
-					tmp[table_header[index].text] = float(player[index].text)
-				else: tmp[table_header[index].text] = player[index].text.replace('-', ' - ')
-			player_dict.append(tmp)
+    # Push to an ordered dictionary, then write each player record to csv
+    with open(file_name, 'w') as outfile:
+        writer = csv.writer(outfile)
+        player_dict = []
+        for player in players:
+            tmp = OrderedDict()
+            for index, player in enumerate(player):
+                if isinstance(player.text, float):
+                    tmp[table_header[index].text] = float(player.text)
+                else:
+                    tmp[table_header[index].text] = player.text.replace('-', ' - ')
+            player_dict.append(tmp)
 
-		#write header to csv
-		writer.writerow(tmp.keys())
+        # Write header to csv
+        writer.writerow(tmp.keys())
 
-		#write data to csv
-		for pl in player_dict:
-			writer.writerow(pl.values())
+        # Write data to csv
+        for pl in player_dict:
+            writer.writerow(pl.values())
 
-	outfile.close()
-
-	#track overall progress
-	current += 1
-	util.update_progress(100 * current / max)
+    outfile.close()
--- bin/util.py
+++ bin/util.py
@@ -1,43 +1,67 @@
+"""
+Utility functions for wikitable stat parsing
+"""
+import sys
+import requests
 from lxml import etree
-import requests, pdb, sys
+
 
 def parse_table_header(query_data):
-	page = requests.get(query_data.page_link)
-	tree = etree.fromstring(page.text)
-	table_header = tree.xpath(query_data.table_path + '//th')
+    """
+    Parses out the header of the stat table
+    """
+    page = requests.get(query_data.page_link)
+    tree = etree.fromstring(page.text)
+    table_header = tree.xpath(query_data.table_path + '//th')
 
-	return table_header
+    return table_header
+
 
 def parse_table_data(query_data):
-	page = requests.get(query_data.page_link)
-	tree = etree.fromstring(page.text)
-	table_data = tree.xpath(query_data.table_path + '//tr/td')
+    """
+    Parses out the data of the stat table
+    """
+    page = requests.get(query_data.page_link)
+    tree = etree.fromstring(page.text)
+    table_data = tree.xpath(query_data.table_path + '//tr/td')
 
-	return table_data
+    return table_data
 
-def chunk_player_data(table_data = [], cols = int):
-	players = []
 
-	for x in range(0, len(table_data), cols):
-		player = table_data[x:x+cols]
-		players.append(player)
+def chunk_player_data(table_data=[], cols=int):
+    """
+    Chunks out rows from stat table
+    """
+    players = []
 
-	return players
+    for _ in range(0, len(table_data), cols):
+        player = table_data[_:_+cols]
+        players.append(player)
+
+    return players
+
 
 def get_csv_file(query_data):
-	file = 'data/' + str(query_data.year) + '_' + str(query_data.league)
-	if(query_data.table == 1):
-		return  file + '_quarterbacks.csv'
-	elif(query_data.table == 2):
-		return file + '_running_backs.csv'
-	elif(query_data.table == 3):
-		return file + '_wide_receivers.csv'
-	elif(query_data.table == 4):
-		return file + '_defenders.csv'
-	elif(query_data.table == 5):
-		return file + '_kickers.csv'
-	elif(query_data.table == 6):
-		return file + '_returners.csv'
+    """
+    Generates CSV file to save data set to
+    """
+    file = 'data/' + str(query_data.year) + '_' + str(query_data.league)
+    if query_data.table == 1:
+        return  file + '_quarterbacks.csv'
+    elif query_data.table == 2:
+        return file + '_running_backs.csv'
+    elif query_data.table == 3:
+        return file + '_wide_receivers.csv'
+    elif query_data.table == 4:
+        return file + '_defenders.csv'
+    elif query_data.table == 5:
+        return file + '_kickers.csv'
+    elif query_data.table == 6:
+        return file + '_returners.csv'
+
 
 def update_progress(progress):
-	sys.stdout.write('\r[{0}] {1}%'.format('#'*(progress/10), progress))
+    """
+    Progress bar
+    """
+    sys.stdout.write('\r[{0}] {1}%'.format('#'*(progress/10), progress))
